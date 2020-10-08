670706341386d3de1e39391a4e181690ca559758
Ian Gibbs <ian.gibbs@virtualclarity.com>
2020-10-08 11:48:17 +0100

    Add pod start

--- lib/Google/Chat/WebHooks.pm
+++ lib/Google/Chat/WebHooks.pm
@@ -1,82 +1,74 @@
 package Google::Chat::WebHooks;
 use strict;
+use warnings;
+use LWP::UserAgent;
+use Class::Tiny qw(room_webhook_url _ua);
+use Carp;
 
 BEGIN {
-    use Exporter ();
-    use vars qw($VERSION @ISA @EXPORT @EXPORT_OK %EXPORT_TAGS);
     $VERSION     = '0.01';
-    @ISA         = qw(Exporter);
-    #Give a hoot don't pollute, do not export more than needed by default
-    @EXPORT      = qw();
-    @EXPORT_OK   = qw();
-    %EXPORT_TAGS = ();
 }
 
-
-#################### subroutine header begin ####################
-
-=head2 sample_function
-
- Usage     : How to use this function/method
- Purpose   : What it does
- Returns   : What it returns
- Argument  : What it wants to know
- Throws    : Exceptions and other anomolies
- Comment   : This is a sample subroutine header.
-           : It is polite to include more pod and fewer comments.
-
-See Also   :
-
-=cut
+sub BUILD
+{	
+	my ($self, $args) = @_;
+	
+	$self->_ua = LWP::UserAgent->new;
+	$self->_ua->timeout(10);
+	$self->_ua->env_proxy;
+	
+	croak "parameter 'room_webhook_url' must be supplied to new" unless $self->room_webhook_url;
+}
 
 #################### subroutine header end ####################
 
-
-sub new
-{
-    my ($class, %parameters) = @_;
-
-    my $self = bless ({}, ref ($class) || $class);
-
-    return $self;
-}
-
+			#~ my $msg_json = "{\"text\": \"$msg\"}";
+			#~ my $req = HTTP::Request->new('POST', $gc_hook);
+			#~ $req->header('Content-Type' => 'application/json');
+			#~ $req->content($msg_json);
+			#~ my $response = $ua->request($req);
+			#~ if($response->code !~ /^[2|3]/)
+			#~ {
+				#~ my $json = decode_json($response->decoded_content());
+				#~ print Dumper $json;
+				#~ exit 2;
+			#~ }
 
 #################### main pod documentation begin ###################
-## Below is the stub of documentation for your module.
-## You better edit it!
-
-
-=head1 NAME
-
-Google::Chat::WebHooks - Send notifications to Google Chat Rooms
 
 =head1 SYNOPSIS
 
   use Google::Chat::WebHooks;
-  blah blah blah
 
+  my $room = Google::Chat::WebHooks->new(room_webhook_url => 'https://chat.googleapis.com/v1/spaces/someid/messages?key=something&token=something');
+  $room->simple_text("This is a message");
+  $room->simple_text("Message with some *bold*");
 
 =head1 DESCRIPTION
 
-Stub documentation for this module was created by ExtUtils::ModuleMaker.
-It looks like the author of the extension was negligent enough
-to leave the stub unedited.
-
-Blah blah blah.
-
+Google::Chat::WebHooks - Send notifications to Google Chat Rooms as provided by G-Suite. Does not work with Google Hangouts as used with your free Google account. Cannot receive messages - for that you need a bot. I'm sure I'll write that module some day. 
 
 =head1 USAGE
 
+Just create an object, passing the webhook URL of the room to which you want to send notifications. Then fire away. If you need help setting up a webhook for your room, see L<https://developers.google.com/hangouts/chat/how-tos/webhooks>.
 
+=over 3
+=item new(room_webhook_url => value)
 
-=head1 BUGS
+Create a new instance of this class, passing in the webhook URL to send messages to. This argument is mandatory. Failure to set it upon creation will result in the method croaking. 
 
+=item simple_text(string)
 
+Send a message to the room. L<Basic formatting is available|https://developers.google.com/hangouts/chat/how-tos/webhooks>.
 
-=head1 SUPPORT
+=item room_webhook_url(), room_webhook_url(value)
 
+Get/set the URL of the room. 
 
+=back
+=head1 BUGS & SUPPORT
+
+Please log them L<on GitHub|https://github.com/realflash/perl-google-chat-webhoooks/issues>.
 
 =head1 AUTHOR
 
@@ -90,15 +82,10 @@ Blah blah blah.
 This program is free software licensed under the...
 
 	The General Public License (GPL)
-	Version 2, June 1991
+	Version 3
 
 The full text of the license can be found in the
 LICENSE file included with this module.
-
-
-=head1 SEE ALSO
-
-perl(1).
 
 =cut
 
